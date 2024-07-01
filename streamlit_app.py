89a5ccb6fb8b41cb913701aa8528c3e70fb01191
Ibrahim Mustafa <85252957+Ibrahimmustafa1@users.noreply.github.com>
2024-07-01 09:50:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,65 @@
 import streamlit as st
-from hugchat import hugchat
-from hugchat.login import Login
+from groq import Groq
+import json
 
-EMAIL = st.secrets["EMAIL"]
-PASSWD = st.secrets["PASSWD"]
-cookie_path_dir = "./cookies/"
+api_key = st.secrets['token']
+client = Groq(api_key=api_key)
 
-# Login and get cookies
-sign = Login(EMAIL, PASSWD)
-cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
+def get_response(query, model, temp):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": query,
+            }
+        ],
+        model=model,
+        temperature=temp,
+        stream=True
+    )
+    for token in chat_completion:
+        msg = token.choices[0].json()
+        msg = json.loads(msg)
+        msg = msg['delta']['content']
+        if msg is not None:
+            yield msg
 
-def get_response(user_input):
-    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
-    for resp in chatbot.query(user_input, stream=True):
-        if resp is not None:
-            yield resp['token']
+with st.sidebar:
+    model_options = {
+        "Gemma 7b": 'gemma-7b-it',
+        "Mixtral 8x7b": 'mixtral-8x7b-32768',
+        "LLaMA3 70b": 'llama3-70b-8192',
+        "LLaMA3 8b": 'llama3-8b-8192'
+    }
     
-    
+    if "selected_model" not in st.session_state:
+        st.session_state.selected_model = list(model_options.keys())[0]
+
+    selected_model = st.selectbox("Select a model", list(model_options.keys()), index=list(model_options.keys()).index(st.session_state.selected_model))
+    selected_model_id = model_options[selected_model]
+    temp = st.slider("Model Temperature", 0.0, 2.0, 0.5, 0.1)
+
+    if selected_model != st.session_state.selected_model:
+        st.session_state.selected_model = selected_model
+        st.session_state.messages = []
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    
 
 for msg in st.session_state.messages:
-    if len(st.session_state.messages) > 0:
-        st.chat_message(msg["role"]).write(msg["content"])
-    
+    st.chat_message(msg["role"]).write(msg["content"])
 
-
-user_input = st.chat_input("Enter your message:", key="user_input")
-st.session_state.messages.append({"role": "user", "content": user_input})
+user_input = st.chat_input("Enter your message:")
 
 if user_input:
+    st.session_state.messages.append({"role": "user", "content": user_input})
     st.chat_message("user").write(user_input)
     placeholder = st.chat_message("AI").empty()
     with st.spinner("Thinking..."):
-        stream_res=""
-        conversation_history = "\n".join([f"user: {msg['role']}\nAI: {msg['content']}" for msg in st.session_state.messages])
+        stream_res = ""
+        conversation_history = "\n".join([f"{msg['role']}: {msg['content']}" for msg in st.session_state.messages])
         combined_input = f"{conversation_history}\nuser: {user_input}\nAI:"
-        for response in get_response(combined_input):
+        for response in get_response(combined_input, selected_model_id, temp):
             stream_res += response
-            placeholder.markdown(stream_res)    
+            placeholder.markdown(stream_res)
         st.session_state.messages.append({"role": "AI", "content": stream_res})
-            
-            
